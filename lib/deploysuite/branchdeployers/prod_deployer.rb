0dde9070cdb4ad4262bd327ce32d5d5e5dd91690
Marc Mentis <mmentis@optonline.net>
2015-03-28 17:25:45 -0400

    Refine clone sql generation

--- lib/deploysuite/branchdeployers/prod_deployer.rb
+++ lib/deploysuite/branchdeployers/prod_deployer.rb
@@ -3,7 +3,10 @@ module Deploysuite
 		include CommonDeployer
 
 		def clone_app_db_functions(args)
+			r.run_load_schema
 			r.run_db_structure_dump
+			r.run_db_rollback_all_migrations
+			r.run_generate_sql_script
 		end
 
 		def update_app_db_functions(args)
