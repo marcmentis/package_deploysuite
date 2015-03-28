689e29efed27667d0e07bb7522c303dece55ef4c
Marc Mentis <mmentis@optonline.net>
2015-03-28 16:10:34 -0400

    Add db:structure:dump for qa/prod

--- lib/deploysuite/branchdeployers/prod_deployer.rb
+++ lib/deploysuite/branchdeployers/prod_deployer.rb
@@ -3,14 +3,7 @@ module Deploysuite
 		include CommonDeployer
 
 		def clone_app_db_functions(args)
-			# puts "PENDING: NEED TO CHECK WITH MULTIPLE MIGRATIONS"
-			# db:structure:dump ? WHAT IS THE STRUCTURE FILE and its purpose
-				# Check if multiple migrations in original will translate into
-				# the same number in the clone - i.e., PROBLEM if VERY MANY -brittle
-				# OR if there is only one migration because 'db:shema:load' was used
-			r.run_load_schema
-			r.run_db_rollback_all_migrations
-			r.run_generate_sql_script
+			r.run_db_structure_dump
 		end
 
 		def update_app_db_functions(args)
