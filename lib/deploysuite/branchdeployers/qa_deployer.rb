d7a55228b97993efafbe0022cef32d8c43612987
Marc Mentis <mmentis@optonline.net>
2015-03-29 15:54:51 -0400

    Modify clone_new_app sql scripts

--- lib/deploysuite/branchdeployers/qa_deployer.rb
+++ lib/deploysuite/branchdeployers/qa_deployer.rb
@@ -8,11 +8,20 @@ module Deploysuite
 			r.run_make_temp_schema
 			r.run_db_rollback_all_migrations			
 			r.run_restore_old_schema
-			r.run_generate_sql_script
+			# r.run_generate_sql_script
+
+			# Better method. Just give structure.sql AFTER 
+				# creating db tables normally
+			# r.run_load_schema
+			# r.run_db_structure_dump
 		end
 
 		def update_app_db_functions(args)
 			r.run_generate_sql_script
+
+			# Better method. Just give sql script after
+				# crating db tables normally
+			# r.run_generate_db_and_sql_script
 		end
 
 		
